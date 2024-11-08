28ce21b8cee930a50845491b78a944e60582c09a
Ju Pan <panju0826@gmail.com>
2024-11-07 23:35:56 -0800

    completed 3.4.2

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+
+
+const app = express()
+app.use(express.json());
+Hello(app);
+Lab5(app);
+
+
+
+app.listen(process.env.PORT || 4000)
