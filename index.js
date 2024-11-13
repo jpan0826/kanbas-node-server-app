6c53d9d357e99f0055f35e0de4e6ff652ac5e872
Ju Pan <panju0826@gmail.com>
2024-11-12 23:25:00 -0800

    completed 4.2.5

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ import express from 'express';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
-
-
+import UserRoutes from "./Kanbas/Users/routes.js";
 
 const app = express();
-app.use(cors());  
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    })
+   );
 app.use(express.json());
 Hello(app);
 Lab5(app);
 
-
-
 app.listen(process.env.PORT || 4000)
+UserRoutes(app);
