dcd48937afa8db43f7678c06f5d301f324c9cc42
Ju Pan <panju0826@gmail.com>
2024-11-18 15:52:14 -0800

    completed 4.5.4

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,4 +1,6 @@
 import * as dao from "./dao.js";
+import * as modulesDao from "../Modules/dao.js";
+
 export default function CourseRoutes(app) {
     app.get("/api/courses", (req, res) => {
         const courses = dao.findAllCourses();
@@ -16,9 +18,27 @@ export default function CourseRoutes(app) {
         const courseUpdates = req.body;
         dao.updateCourse(courseId, courseUpdates);
         res.sendStatus(204);
-    })
+    });
 
+    app.post("/api/courses/:courseId/modules", (req, res) => {
+        const { courseId } = req.params;
+        const module = {
+          ...req.body,
+          course: courseId,
+        };
+        const newModule = modulesDao.createModule(module);
+        res.send(newModule);
+      });
     
+
+    app.get("/api/courses/:courseId/modules", (req, res) => {
+        const { courseId } = req.params;
+        const modules = modulesDao.findModulesForCourse(courseId);
+        res.json(modules);
+    });
+
+
+
 
 }
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from './Kanbas/Courses/routes.js';
+import ModuleRoutes from './Kanbas/Modules/routes.js';
 import session from "express-session";
 import "dotenv/config";
 
@@ -37,5 +38,6 @@ UserRoutes(app);
 CourseRoutes(app);
 Hello(app);
 Lab5(app);
+ModuleRoutes(app);
 
 app.listen(process.env.PORT || 4000)
